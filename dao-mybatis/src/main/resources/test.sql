d9cbb0f78079848272f60f41d28f7bd61d62360f
r720 <963838470@qq.com>
2019-11-28 00:03:23 +0800

    Mybatis Plus CodeGenerator

--- dao-mybatis/src/main/resources/test.sql
+++ dao-mybatis/src/main/resources/test.sql
@@ -7,5 +7,11 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+ALTER TABLE user COMMENT '用户';
+ALTER TABLE user MODIFY COLUMN id VARCHAR(100) COMMENT '主键id';
+ALTER TABLE user MODIFY COLUMN userName VARCHAR(100) COMMENT '用户名';
+ALTER TABLE user MODIFY COLUMN passWord VARCHAR(100) COMMENT '密码';
+ALTER TABLE user MODIFY COLUMN realName VARCHAR(100) COMMENT '真实姓名';
+
 INSERT into `user` (id,userName,`passWord`,realName) values (1,'张三丰','123','张三');
 INSERT into `user` (id,userName,`passWord`,realName) values (2,'李四光','456','李四');
