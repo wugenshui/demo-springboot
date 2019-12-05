cfe6b9f6dea637a344c6a5b14eb46464c832e47b
r720 <963838470@qq.com>
2019-12-05 23:58:03 +0800

    swagger配置

--- dao-mybatis-plus/src/main/java/com/chenbo/daomybatisplus/auth/controller/UserController.java
+++ dao-mybatis-plus/src/main/java/com/chenbo/daomybatisplus/auth/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.chenbo.daomybatisplus.auth.controller;
 
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -13,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chenbo
  * @since 2019-12-01
  */
+@Api("用户")
 @RestController
 @RequestMapping("/auth/user")
 public class UserController {
 
+    @GetMapping("")
+    public LocalDateTime getUser(LocalDateTime dateTime) {
+        return dateTime;
+    }
 }
 
