ed4c8145cec999b2d6a3ee1cafece39c2987f6a5
r720 <963838470@qq.com>
2019-12-23 23:10:00 +0800

    分页查询 + 事务控制，测试完成回滚数据

--- dao-hikari/src/test/java/com/chenbo/daohikari/DaoHikariApplicationTests.java
+++ dao-hikari/src/test/java/com/chenbo/daohikari/DaoHikariApplicationTests.java
@@ -2,28 +2,54 @@ package com.chenbo.daohikari;
 
 import com.chenbo.daohikari.domain.User;
 import com.chenbo.daohikari.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
+@Rollback
 public class DaoHikariApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
 
     @Test
-    public void contextLoads() {
-    }
-
-    @Test
     public void testSelectAll() {
         List<User> users = userMapper.selectAll();
         users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testPage() {
+        PageHelper.startPage(1, 5);
+        PageInfo<User> userPageInfo = new PageInfo<User>(userMapper.selectAll());
+        System.out.println("userPageInfo = " + userPageInfo);
+    }
+
+    @Test
+    public void testInsert() {
+        User user = new User();
+        user.setId(123213L);
+        user.setName("");
+        user.setAge(0);
+        user.setEmail("");
+        user.setManagerId(1087982257332887553L);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        user.setVersion(0);
+        user.setDeleted(0);
+
+        userMapper.insert(user);
     }
 }
