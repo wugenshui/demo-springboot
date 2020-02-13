17f26cf66be23bc0c650d4d46c1dd60f0c3e986c
r720 <963838470@qq.com>
2020-02-13 15:45:34 +0800

    新增最佳实践项目

---
+++ best-practice/src/main/java/com/chenbo/best/practice/BestPracticeApplication.java
@@ -0,0 +1,19 @@
+package com.chenbo.best.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 最佳实践项目
+ *
+ * @author : chenbo
+ * @date : 2020-02-13
+ */
+@SpringBootApplication
+public class BestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPracticeApplication.class, args);
+    }
+
+}
---
+++ best-practice/src/test/java/com/chenbo/best/practice/BestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenbo.best.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
