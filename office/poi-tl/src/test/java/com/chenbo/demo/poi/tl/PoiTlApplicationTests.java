259194083833c12a2977b2a76eb66f4029cfb231
chenbo <963838470@qq.com>
2020-03-06 23:52:27 +0800

    文件路径修改

--- office/poi-tl/src/test/java/com/chenbo/demo/poi/tl/PoiTlApplicationTests.java
+++ office/poi-tl/src/test/java/com/chenbo/demo/poi/tl/PoiTlApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -15,19 +14,23 @@ import java.util.HashMap;
 public class PoiTlApplicationTests {
 
     @Test
-    public void contextLoads() throws IOException {
-        String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        System.out.println("path = " + path);
-        XWPFTemplate template = XWPFTemplate.compile(path + "/template.docx")
+    public void wordExportTest() throws IOException {
+        XWPFTemplate.compile("src/test/resources/template.docx")
                 .render(new HashMap<String, Object>() {{
                     put("title", "Poi-tl 模板引擎标题");
                     put("content", "这里是正文");
-                }});
-        FileOutputStream out = new FileOutputStream(path + "/out_template.docx");
-        template.write(out);
-        out.flush();
-        out.close();
-        template.close();
+                }})
+                .writeToFile("out_template.docx");
     }
+
+//    @Test
+//    public void dbWordTest() throws IOException {
+//        XWPFTemplate.compile("src/test/resources/db.docx")
+//                .render(new HashMap<String, Object>() {{
+//                    put("title", "Poi-tl 模板引擎标题");
+//                    put("content", "这里是正文");
+//                }})
+//                .writeToFile("out_db.docx");
+//    }
 
 }
