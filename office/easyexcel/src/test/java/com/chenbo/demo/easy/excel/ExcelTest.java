69544ffa69f5b8fc53f5893495e442b70d5a90de
chenbo <963838470@qq.com>
2020-03-08 23:38:02 +0800

    easyexcel 写入excel

--- office/easyexcel/src/test/java/com/chenbo/demo/easy/excel/ExcelTest.java
+++ office/easyexcel/src/test/java/com/chenbo/demo/easy/excel/ExcelTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author : chenbo
@@ -29,6 +32,34 @@ public class ExcelTest {
 
     @Test
     public void writeExcelTest() {
+        String fileName = ExcelTest.class.getResource("/").getPath() + File.separator + "writer.xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName).head(head()).sheet("模板").doWrite(dataList());
+    }
 
+    private List<List<String>> head() {
+        List<List<String>> list = new ArrayList<List<String>>();
+        List<String> head0 = new ArrayList<String>();
+        head0.add("字符串" + System.currentTimeMillis());
+        List<String> head1 = new ArrayList<String>();
+        head1.add("数字" + System.currentTimeMillis());
+        List<String> head2 = new ArrayList<String>();
+        head2.add("日期" + System.currentTimeMillis());
+        list.add(head0);
+        list.add(head1);
+        list.add(head2);
+        return list;
+    }
+
+    private List<List<Object>> dataList() {
+        List<List<Object>> list = new ArrayList<List<Object>>();
+        for (int i = 0; i < 10; i++) {
+            List<Object> data = new ArrayList<Object>();
+            data.add("字符串" + i);
+            data.add(new Date());
+            data.add(0.56);
+            list.add(data);
+        }
+        return list;
     }
 }
