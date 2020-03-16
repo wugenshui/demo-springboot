62a5967d9fc57471843804eec165b0878050455e
chenbo <963838470@qq.com>
2020-03-16 23:34:25 +0800

    curl调用接口报错

--- socket/netty/src/main/java/com/chenbo/demo/netty/firstexample/TestHttpServerHandler.java
+++ socket/netty/src/main/java/com/chenbo/demo/netty/firstexample/TestHttpServerHandler.java
@@ -8,6 +8,7 @@ import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.CharsetUtil;
@@ -19,7 +20,7 @@ import io.netty.util.CharsetUtil;
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        if (msg instanceof HttpObject) {
+        if (msg instanceof HttpRequest) {
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
