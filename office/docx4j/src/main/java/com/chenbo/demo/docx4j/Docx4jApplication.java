2aedeb92b83aca14f4800ddcc1726d359d5a2419
wugenshui <963838470@qq.com>
2020-03-16 18:29:55 +0800

    docx4j

---
+++ office/docx4j/src/main/java/com/chenbo/demo/docx4j/Docx4jApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.demo.docx4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-03-16
+ */
+@SpringBootApplication
+public class Docx4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Docx4jApplication.class);
+    }
+}
