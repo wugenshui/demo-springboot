556cd7b2646593c5ffa9a570e27c57b78b2a1313
chenbo <963838470@qq.com>
2020-03-19 23:46:55 +0800

    socket连接

---
+++ socket/netty/src/main/java/com/chenbo/demo/netty/secondexample/MyServerHandler.java
@@ -0,0 +1,24 @@
+package com.chenbo.demo.netty.secondexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ * @author : chenbo
+ * @date : 2020-03-19
+ */
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ":" + msg);
+        ctx.writeAndFlush("from server:" + UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
