ad217e5c592ac0741f5905a76acdafa0ae74777e
chenbo <963838470@qq.com>
2020-03-21 16:52:19 +0800

    心跳连接demo 客户端可使用MyChatClient

---
+++ socket/netty/src/main/java/com/chenbo/demo/netty/fourtheexample/MyServerInitializer.java
@@ -0,0 +1,25 @@
+package com.chenbo.demo.netty.fourtheexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author : chenbo
+ * @date : 2020-03-19
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(4, 5, 8));
+        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
