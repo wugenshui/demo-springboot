0899f575441603e9376f734152d5cc8a4be5161c
chenbo <963838470@qq.com>
2020-03-21 23:42:53 +0800

    websocket 服务端

---
+++ socket/netty/src/main/java/com/chenbo/demo/netty/fifthexample/MyServer.java
@@ -0,0 +1,36 @@
+package com.chenbo.demo.netty.fifthexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author : chenbo
+ * @date : 2020-03-19
+ */
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.WARN))
+                    .childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ socket/netty/src/main/java/com/chenbo/demo/netty/fifthexample/MyServerInitializer.java
@@ -0,0 +1,27 @@
+package com.chenbo.demo.netty.fifthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author : chenbo
+ * @date : 2020-03-19
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        pipeline.addLast(new MyServerHandler());
+    }
+}
