9d113658b260ce2ad99bfb5db9e4a3404d56b67b
wugenshui <963838470@qq.com>
2020-03-24 16:57:08 +0800

    io操作

--- base-util/src/test/java/com/chenbo/baseutil/java/IOTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/java/IOTest.java
@@ -6,12 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * @author : chenbo
@@ -38,7 +33,21 @@ public class IOTest {
     @Test
     public void InputStreamTest() throws Exception {
         FileInputStream fileInputStream = new FileInputStream("output.txt");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+        //dataInputStream.
+        byte[] b = new byte[20];
+        int len;
+        while ((len = dataInputStream.read(b)) != -1) {
+            String str = new String(b);
+            System.out.println(str);
+            System.out.println(len);
+        }
+
+        dataInputStream.close();
+
+        // 推荐方式
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("output.txt")));
         System.out.println(bufferedReader.readLine());
 
         bufferedReader.close();
