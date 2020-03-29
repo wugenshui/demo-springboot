81a4e4b1e0b6cf09e30a8cb5b0547ecb0ab2f9e9
chenbo <chenbo@huaweisoft.com>
2020-03-29 10:05:49 +0800

    测试方法调整

--- base-util/src/main/java/com/chenbo/baseutil/util/GpsConvertUtil.java
+++ base-util/src/main/java/com/chenbo/baseutil/util/GpsConvertUtil.java
@@ -7,8 +7,8 @@ package com.chenbo.baseutil.util;
  * WGS84坐标系：大地坐标系，GPS原始坐标系     目前广泛使用的GPS全球卫星定位系统使用的坐标系（国外）；Google地球<br />
  * GCJ02坐标系：国测局坐标系，火星坐标系      由中国国家测绘局制定的地理坐标系统，是由WGS84加密后得到的坐标系；高德、腾讯、Google中国<br />
  * BD09坐标系：百度地图采用的坐标系           在GCJ02坐标系基础上再次加密<br />
- * longitude    经度(国内范围3.51-53.33)<br />
- * latitude     纬度(国内范围73.33-135.05)<br />
+ * longitude    经度(国内范围73.33-135.05)<br />
+ * latitude     纬度(国内范围3.51-53.33)<br />
  * </p>
  *
  * @author : chenbo
@@ -36,33 +36,33 @@ public class GpsConvertUtil {
     /**
      * 中国经度下限
      */
-    private static final double CHINA_LON_LOWER = 72.004;
+    private static final double CHINA_LNG_LOWER = 72.004;
     /**
      * 中国经度上限
      */
-    private static final double CHINA_LON_UPPER = 137.8347;
+    private static final double CHINA_LNG_UPPER = 137.8347;
 
     /**
      * WGS84(大地坐标系) 转 GCJ02(火星坐标系)
      *
-     * @param lat 纬度
-     * @param lon 经度
+     * @param lng 纬度
+     * @param lat 经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] wgs84ToGcj02(double lat, double lon) {
-        if (outOfChina(lat, lon)) {
-            return new double[]{lat, lon};
+    public static double[] wgs84ToGcj02(double lat, double lng) {
+        if (outOfChina(lat, lng)) {
+            return new double[]{lat, lng};
         }
-        double dLat = transformLat(lon - 105.0, lat - 35.0);
-        double dLon = transformLon(lon - 105.0, lat - 35.0);
+        double dLat = transformLat(lng - 105.0, lat - 35.0);
+        double dLng = transformLng(lng - 105.0, lat - 35.0);
         double radLat = lat / 180.0 * PI;
         double magic = Math.sin(radLat);
         magic = 1 - EE * magic * magic;
         double sqrtMagic = Math.sqrt(magic);
         dLat = (dLat * 180.0) / ((A * (1 - EE)) / (magic * sqrtMagic) * PI);
-        dLon = (dLon * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
+        dLng = (dLng * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
         double mgLat = lat + dLat;
-        double mgLon = lon + dLon;
+        double mgLon = lng + dLng;
         return new double[]{mgLat, mgLon};
     }
 
@@ -70,12 +70,12 @@ public class GpsConvertUtil {
      * GCJ02(火星坐标系) 转 WGS84(大地坐标系)
      *
      * @param gcjLat GCJ02纬度
-     * @param gcjLon GCJ02经度
+     * @param gcjLng GCJ02经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] gcj02ToWgs84(double gcjLat, double gcjLon) {
-        double[] gps = transform(gcjLat, gcjLon);
-        double lontitude = gcjLon * 2 - gps[1];
+    public static double[] gcj02ToWgs84(double gcjLat, double gcjLng) {
+        double[] gps = transform(gcjLat, gcjLng);
+        double lontitude = gcjLng * 2 - gps[1];
         double latitude = gcjLat * 2 - gps[0];
         return new double[]{latitude, lontitude};
     }
@@ -84,43 +84,43 @@ public class GpsConvertUtil {
      * GCJ02(火星坐标系) 转 BD09(百度坐标系)
      *
      * @param gcjLat GCJ02纬度
-     * @param gcjLon GCJ02经度
+     * @param gcjLng GCJ02经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] gcj02ToBd09(double gcjLat, double gcjLon) {
-        double z = Math.sqrt(gcjLon * gcjLon + gcjLat * gcjLat) + 0.00002 * Math.sin(gcjLat * X_PI);
-        double theta = Math.atan2(gcjLat, gcjLon) + 0.000003 * Math.cos(gcjLon * X_PI);
-        double tempLon = z * Math.cos(theta) + 0.0065;
+    public static double[] gcj02ToBd09(double gcjLat, double gcjLng) {
+        double z = Math.sqrt(gcjLng * gcjLng + gcjLat * gcjLat) + 0.00002 * Math.sin(gcjLat * X_PI);
+        double theta = Math.atan2(gcjLat, gcjLng) + 0.000003 * Math.cos(gcjLng * X_PI);
+        double tempLng = z * Math.cos(theta) + 0.0065;
         double tempLat = z * Math.sin(theta) + 0.006;
-        return new double[]{tempLat, tempLon};
+        return new double[]{tempLat, tempLng};
     }
 
     /**
      * BD09(百度坐标系) 转 GCJ02(火星坐标系)
      *
      * @param bdLat BD09纬度
-     * @param bdLon BD09经度
+     * @param bdLng BD09经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] bd09ToGcj02(double bdLat, double bdLon) {
-        double x = bdLon - 0.0065;
+    public static double[] bd09ToGcj02(double bdLat, double bdLng) {
+        double x = bdLng - 0.0065;
         double y = bdLat - 0.006;
         double z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * X_PI);
         double theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * X_PI);
-        double tempLon = z * Math.cos(theta);
+        double tempLng = z * Math.cos(theta);
         double tempLat = z * Math.sin(theta);
-        return new double[]{tempLat, tempLon};
+        return new double[]{tempLat, tempLng};
     }
 
     /**
      * WGS84(大地坐标系) 转 BD09(百度坐标系)
      *
      * @param lat 纬度
-     * @param lon 经度
+     * @param lng 经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] wgs84ToBd09(double lat, double lon) {
-        double[] gcj02 = wgs84ToGcj02(lat, lon);
+    public static double[] wgs84ToBd09(double lat, double lng) {
+        double[] gcj02 = wgs84ToGcj02(lat, lng);
         return gcj02ToBd09(gcj02[0], gcj02[1]);
     }
 
@@ -128,11 +128,11 @@ public class GpsConvertUtil {
      * BD09(百度坐标系) 转 WGS84(大地坐标系)
      *
      * @param bdLat BD09纬度
-     * @param bdLon BD09经度
+     * @param bdLng BD09经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] bd09ToWgs84(double bdLat, double bdLon) {
-        double[] gcj02 = bd09ToGcj02(bdLat, bdLon);
+    public static double[] bd09ToWgs84(double bdLat, double bdLng) {
+        double[] gcj02 = bd09ToGcj02(bdLat, bdLng);
         double[] gps84 = gcj02ToWgs84(gcj02[0], gcj02[1]);
         // 保留小数点后六位
         gps84[0] = retain6(gps84[0]);
@@ -144,35 +144,35 @@ public class GpsConvertUtil {
      * 经纬度转墨卡托投影坐标系
      *
      * @param lat 纬度
-     * @param lon 经度
+     * @param lng 经度
      * @return 经纬度数组 [纬度，经度]
      */
-    public static double[] lonLatToMercator(double lat, double lon) {
+    public static double[] lngLatToMercator(double lat, double lng) {
         double newLat = (lat * 20037508.342789 / 180);
-        double newLon = (Math.log(Math.tan((90 + lon) * Math.PI / 360)) / (Math.PI / 180));
-        newLon = newLon * 20037508.342789 / 180;
-        return new double[]{newLat, newLon};
+        double newLng = (Math.log(Math.tan((90 + lng) * Math.PI / 360)) / (Math.PI / 180));
+        newLng = newLng * 20037508.342789 / 180;
+        return new double[]{newLat, newLng};
     }
 
     /**
      * 获取两个经纬度的距离,单位：米
      *
-     * @param longitude1 经度1
      * @param latitude1  纬度1
-     * @param longitude2 经度2
+     * @param longitude1 经度1
      * @param latitude2  纬度2
+     * @param longitude2 经度2
      * @return 距离，单位：米
      */
-    public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
-        double lon1 = (Math.PI / 180) * longitude1;
-        double lon2 = (Math.PI / 180) * longitude2;
+    public static double getDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
         double lat1 = (Math.PI / 180) * latitude1;
+        double lng1 = (Math.PI / 180) * longitude1;
         double lat2 = (Math.PI / 180) * latitude2;
+        double lng2 = (Math.PI / 180) * longitude2;
         // 地球半径
         double r = 6371;
         // 两点间距离 km
-        double d = Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)) * r;
-        //返回米
+        double d = Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1)) * r;
+        // 返回单位米
         return d * 1000;
     }
 
@@ -184,7 +184,7 @@ public class GpsConvertUtil {
         return ret;
     }
 
-    private static double transformLon(double x, double y) {
+    private static double transformLng(double x, double y) {
         double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
         ret += (20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0 / 3.0;
         ret += (20.0 * Math.sin(x * PI) + 40.0 * Math.sin(x / 3.0 * PI)) * 2.0 / 3.0;
@@ -192,25 +192,25 @@ public class GpsConvertUtil {
         return ret;
     }
 
-    private static double[] transform(double lat, double lon) {
-        if (outOfChina(lat, lon)) {
-            return new double[]{lat, lon};
+    private static double[] transform(double lat, double lng) {
+        if (outOfChina(lat, lng)) {
+            return new double[]{lat, lng};
         }
-        double dLat = transformLat(lon - 105.0, lat - 35.0);
-        double dLon = transformLon(lon - 105.0, lat - 35.0);
+        double dLat = transformLat(lng - 105.0, lat - 35.0);
+        double dLng = transformLng(lng - 105.0, lat - 35.0);
         double radLat = lat / 180.0 * PI;
         double magic = Math.sin(radLat);
         magic = 1 - EE * magic * magic;
         double sqrtMagic = Math.sqrt(magic);
         dLat = (dLat * 180.0) / ((A * (1 - EE)) / (magic * sqrtMagic) * PI);
-        dLon = (dLon * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
+        dLng = (dLng * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
         double mgLat = lat + dLat;
-        double mgLon = lon + dLon;
-        return new double[]{mgLat, mgLon};
+        double mgLng = lng + dLng;
+        return new double[]{mgLat, mgLng};
     }
 
-    private static boolean outOfChina(double lat, double lon) {
-        if (lon < CHINA_LON_LOWER || lon > CHINA_LON_UPPER) {
+    public static boolean outOfChina(double lat, double lng) {
+        if (lng < CHINA_LNG_LOWER || lng > CHINA_LNG_UPPER) {
             return true;
         }
         return lat < 0.8293 || lat > 55.8271;
--- base-util/src/test/java/com/chenbo/baseutil/util/GpsConvertUtilTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/util/GpsConvertUtilTest.java
@@ -45,95 +45,87 @@ public class GpsConvertUtilTest {
 
     @Test
     public void gpsTransTest() {
-        // 坐标系(纬度lat,经度lon) wgs-84(0,1) gcj-02(2,3) bd-08(4,5)
+        // 坐标系(纬度lat,经度lon)
         List<AllMap> allMaps = new ArrayList<>();
         // 金星大厦
-        allMaps.add(new AllMap(113.34712152, 23.14410896,
-                113.347131, 23.144113,
-                113.35368518, 23.14979204));
+        allMaps.add(new AllMap(23.1466403500, 113.3416945400,
+                23.1440792900, 113.3471489000,
+                23.1497583300, 113.3537030700));
         // 广州塔
-        allMaps.add(new AllMap(113.3244465700, 23.1064679000,
-                113.3244600000, 23.1064700000,
-                113.3309751400, 23.1123809800));
+        allMaps.add(new AllMap(23.1090977200, 113.3190660600,
+                23.1064700000, 113.3244600000,
+                23.1123809800, 113.3309751400));
         // 花城汇
-        allMaps.add(new AllMap(113.3246238100, 23.1201367700,
-                113.3246380000, 23.1201380000,
-                113.3311448400, 23.1260471300));
+        allMaps.add(new AllMap(23.1227614600, 113.3192433000,
+                23.1201380000, 113.3246380000,
+                23.1260471300, 113.3311448400));
         // 允许的误差
-        double delta = 0.001;
+        double delta = 30;
         double[] transPoint = null;
-        double dValue;
+        double distance;
 
         for (AllMap allMap : allMaps) {
             System.out.println("wgs84ToGcj02");
             transPoint = GpsConvertUtil.wgs84ToGcj02(allMap.wgs.lat, allMap.wgs.lng);
-            dValue = transPoint[0] - allMap.gcj.lat;
-            System.out.println("纬度lat：" + allMap.wgs.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.gcj.lat, delta);
-            dValue = transPoint[1] - allMap.gcj.lng;
-            System.out.println("经度lon：" + allMap.wgs.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.gcj.lng, delta);
-
-            System.out.println("gcj02ToWgs84");
-            transPoint = GpsConvertUtil.gcj02ToWgs84(allMap.gcj.lat, allMap.gcj.lng);
-            dValue = transPoint[0] - allMap.wgs.lat;
-            System.out.println("纬度lat：" + allMap.gcj.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.wgs.lat, delta);
-            dValue = transPoint[1] - allMap.wgs.lng;
-            System.out.println("经度lon：" + allMap.gcj.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.wgs.lng, delta);
-
-            System.out.println("gcj02ToBd09");
-            transPoint = GpsConvertUtil.gcj02ToBd09(allMap.gcj.lat, allMap.gcj.lng);
-            dValue = transPoint[0] - allMap.bd.lat;
-            System.out.println("纬度lat：" + allMap.gcj.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.bd.lat, delta);
-            dValue = transPoint[1] - allMap.bd.lng;
-            System.out.println("经度lon：" + allMap.gcj.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.bd.lng, delta);
-
-            System.out.println("bd09ToGcj02");
-            transPoint = GpsConvertUtil.bd09ToGcj02(allMap.bd.lat, allMap.bd.lng);
-            dValue = transPoint[0] - allMap.gcj.lat;
-            System.out.println("纬度lat：" + allMap.bd.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.gcj.lat, delta);
-            dValue = transPoint[1] - allMap.gcj.lng;
-            System.out.println("经度lon：" + allMap.bd.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.gcj.lng, delta);
+            distance = GpsConvertUtil.getDistance(allMap.gcj.lat, allMap.gcj.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.wgs.lat + "," + allMap.wgs.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
 
             System.out.println("wgs84ToBd09");
             transPoint = GpsConvertUtil.wgs84ToBd09(allMap.wgs.lat, allMap.wgs.lng);
-            dValue = transPoint[0] - allMap.bd.lat;
-            System.out.println("纬度lat：" + allMap.wgs.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.bd.lat, delta);
-            dValue = transPoint[1] - allMap.bd.lng;
-            System.out.println("经度lon：" + allMap.wgs.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.bd.lng, delta);
+            distance = GpsConvertUtil.getDistance(allMap.bd.lat, allMap.bd.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.wgs.lat + "," + allMap.wgs.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
+
+            System.out.println("gcj02ToWgs84");
+            transPoint = GpsConvertUtil.gcj02ToWgs84(allMap.gcj.lat, allMap.gcj.lng);
+            distance = GpsConvertUtil.getDistance(allMap.wgs.lat, allMap.wgs.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.gcj.lat + "," + allMap.gcj.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
+
+            System.out.println("gcj02ToBd09");
+            transPoint = GpsConvertUtil.gcj02ToBd09(allMap.gcj.lat, allMap.gcj.lng);
+            distance = GpsConvertUtil.getDistance(allMap.bd.lat, allMap.bd.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.gcj.lat + "," + allMap.gcj.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
+
+            System.out.println("bd09ToGcj02");
+            transPoint = GpsConvertUtil.bd09ToGcj02(allMap.bd.lat, allMap.bd.lng);
+            distance = GpsConvertUtil.getDistance(allMap.gcj.lat, allMap.gcj.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.bd.lat + "," + allMap.bd.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
 
             System.out.println("bd09ToWgs84");
             transPoint = GpsConvertUtil.bd09ToWgs84(allMap.bd.lat, allMap.bd.lng);
-            dValue = transPoint[0] - allMap.wgs.lat;
-            System.out.println("纬度lat：" + allMap.bd.lat + "->" + transPoint[0] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[0], allMap.wgs.lat, delta);
-            dValue = transPoint[1] - allMap.wgs.lng;
-            System.out.println("经度lon：" + allMap.bd.lng + "->" + transPoint[1] + " 误差：" + dValue);
-            Assert.assertEquals(transPoint[1], allMap.wgs.lng, delta);
+            distance = GpsConvertUtil.getDistance(allMap.wgs.lat, allMap.wgs.lng, transPoint[0], transPoint[1]);
+            System.out.println(allMap.bd.lat + "," + allMap.bd.lng + "->" + transPoint[0] + "," + transPoint[1] + " 误差：" + distance + "米");
+            Assert.assertTrue(distance < delta);
 
             System.out.println("------------------------------------------------------------");
         }
     }
 
     @Test
-    public void mercatorTransTest() {
-        double[] mercator = GpsConvertUtil.lonLatToMercator(113.3473512844152, 23.14398871029491);
-        Assert.assertEquals(12617769.427747251, mercator[0], 0);
-        Assert.assertEquals(2649440.948904634, mercator[1], 0);
+    public void outOfChinaTest() {
+        double lon = 113.34736228460538;
+        double lat = 23.144000709888573;
+        double[] a = GpsConvertUtil.gcj02ToWgs84(lat, lon);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+
+
+        List<double[]> points = new ArrayList<>();
+        points.add(new double[]{113.34712152, 23.14410896});
+        points.add(new double[]{113.3244600000, 23.1064700000});
+        for (double[] point : points) {
+            Assert.assertFalse(GpsConvertUtil.outOfChina(point[0], point[1]));
+        }
     }
 
     @Test
-    public void distanceTest() {
-
-        double distance = GpsConvertUtil.getDistance(23.14979204, 113.35368518, 23.143511618621396, 113.34764834035583);
-        System.out.println("误差" + distance + "米");
+    public void mercatorTransTest() {
+        double[] mercator = GpsConvertUtil.lngLatToMercator(113.3473512844152, 23.14398871029491);
+        Assert.assertEquals(12617769.427747251, mercator[0], 0);
+        Assert.assertEquals(2649440.948904634, mercator[1], 0);
     }
 }
