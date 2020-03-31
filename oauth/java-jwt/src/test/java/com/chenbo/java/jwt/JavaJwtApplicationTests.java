b166068827775219d121e8d868bb08a43227cc04
chenbo <963838470@qq.com>
2020-03-31 23:52:52 +0800

    测试jwt授权认证

--- oauth/java-jwt/src/test/java/com/chenbo/java/jwt/JavaJwtApplicationTests.java
+++ oauth/java-jwt/src/test/java/com/chenbo/java/jwt/JavaJwtApplicationTests.java
@@ -1,34 +1,44 @@
 package com.chenbo.java.jwt;
 
+import com.alibaba.fastjson.JSONObject;
 import com.chenbo.java.jwt.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class JavaJwtApplicationTests {
 
-    @Test(expected = ResourceAccessException.class)
+    @Test(expected = HttpServerErrorException.class)
     public void noOauthTest() {
         RestTemplate restTemplate = new RestTemplate();
         Object obj = restTemplate.getForObject("http://localhost:8080/api/getMessage", Object.class);
-
-        //Object obj = restTemplate.getForObject("http://www.baidu.com", Object.class);
-        System.out.println("obj = " + obj);
     }
 
     @Test
     public void loginTest() {
         RestTemplate restTemplate = new RestTemplate();
         User user = new User("1", "zhangsan", "123");
-        Object obj = restTemplate.postForEntity("http://localhost:8080/api/login", user, Object.class);
+        ResponseEntity<JSONObject> obj = restTemplate.postForEntity("http://localhost:8080/api/login", user, JSONObject.class);
 
-        //Object obj = restTemplate.getForObject("http://www.baidu.com", Object.class);
         System.out.println("obj = " + obj);
+        JSONObject json = obj.getBody();
+        assert json != null;
+        String token = json.getString("token");
+        System.out.println("token = " + token);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("token", token);
+        ResponseEntity<String> responseGetMessage = restTemplate.exchange("http://localhost:8080/api/getMessage", HttpMethod.GET, new HttpEntity<String>(null, headers), String.class);
+        System.out.println(responseGetMessage.getBody());
     }
 
 }
