5cfb817df0d479e1ed690d1aae5d4239e1b9b9b5
chenbo <chenbo@huaweisoft.com>
2020-04-01 21:59:03 +0800

    编码转换测试

--- base-util/src/test/java/com/chenbo/baseutil/code/CodeTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/code/CodeTest.java
@@ -6,10 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedMap;
 
 /**
  * @author : chenbo
@@ -22,12 +20,7 @@ public class CodeTest {
     public void deCode() throws UnsupportedEncodingException {
         String str = "娌℃湁閭ｄ釜鏂囦欢鎴栫洰褰�";
 
-        SortedMap<String, Charset> map = Charset.availableCharsets();
         List<String> list = new ArrayList<>();
-        //for (String alias : map.keySet()) {
-        //    // 输出字符集的别名
-        //    list.add(alias);
-        //}
         list.add("UTF-8");
         list.add("UTF-16");
         list.add("GBK");
@@ -36,11 +29,12 @@ public class CodeTest {
         list.add("ISO-8859-1");
 
         for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                String result = new String(str.getBytes(list.get(i)), list.get(j));
-                System.out.println(list.get(i) + "->" + list.get(j) + ":" + result);
+            for (int j = 0; j < list.size(); j++) {
+                if (i != j) {
+                    String result = new String(str.getBytes(list.get(i)), list.get(j));
+                    System.out.println(list.get(i) + "编码->" + list.get(j) + "解码:" + result);
+                }
             }
         }
-
     }
 }
