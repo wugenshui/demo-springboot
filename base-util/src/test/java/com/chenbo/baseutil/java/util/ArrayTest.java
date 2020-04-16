ab07ab92ab83e15d50ffada3b4f8a471bbd8432c
chenbo <963838470@qq.com>
2020-04-16 21:58:54 +0800

    遍历耗时测试

--- base-util/src/test/java/com/chenbo/baseutil/java/util/ArrayTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/java/util/ArrayTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author : chenbo
@@ -25,5 +28,29 @@ public class ArrayTest {
         System.out.println(doubleArrayInt);
         System.out.println(Arrays.deepToString(doubleArrayInt));
 
+    }
+
+    @Test
+    public void forTest() {
+        List<Integer> listNumber = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            listNumber.add(i);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start("foreach耗时");
+        listNumber.forEach(i -> {
+
+        });
+        stopWatch.stop();
+
+        stopWatch.start("for耗时");
+        for (int i = 0; i < listNumber.size(); i++) {
+
+        }
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
     }
 }
