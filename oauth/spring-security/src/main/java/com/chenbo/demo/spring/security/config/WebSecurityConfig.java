7091e3d9ccbabc894475d1a3c7660f9e4a961cb7
chenbo <963838470@qq.com>
2020-04-19 21:52:24 +0800

    方法授权

--- oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/config/WebSecurityConfig.java
+++ oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.chenbo.demo.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
@@ -26,8 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "home").permitAll()
-                .antMatchers("/f1").hasAuthority("f1")
-                .antMatchers("/f2").hasAuthority("f2")
+                //.antMatchers("/f1").hasAuthority("f1")
+                //.antMatchers("/f2").hasAuthority("f2")
                 .anyRequest().authenticated().and()
                 .formLogin().loginPage("/login").permitAll().and()
                 .logout().permitAll();
--- oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/controller/HelloController.java
+++ oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.chenbo.demo.spring.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/f1")
+    @PreAuthorize("hasAuthority('f1')")
     public String f1() {
         return "f1";
     }
 
     @GetMapping("/f2")
+    @PreAuthorize("hasAuthority('f2')")
     public String f2() {
         return "f2";
     }
