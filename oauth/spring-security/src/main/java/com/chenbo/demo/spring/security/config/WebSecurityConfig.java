9f19d31815ed3158c589aa33c1c6955e8e312b9c
chenbo <963838470@qq.com>
2020-04-19 18:30:51 +0800

    BCrypt加密

--- oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/config/WebSecurityConfig.java
+++ oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/config/WebSecurityConfig.java
@@ -1,35 +1,44 @@
-//package com.chenbo.demo.spring.security.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-///**
-// * @author : chenbo
-// * @date : 2020-04-19
-// */
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "home").permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login").permitAll()
-//                .and().logout().permitAll();
-//    }
-//
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) {
-//        try {
-//            auth.inMemoryAuthentication()
-//                    .withUser("admin").password("admin").roles("ADMIN")
-//                    .and().withUser("user").password("user").roles("USER");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package com.chenbo.demo.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-19
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "home").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").permitAll().and()
+                .logout().permitAll();
+    }
+
+    /**
+     * 安全拦截配置
+     *
+     * @param auth
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        try {
+            auth.inMemoryAuthentication()
+                    // 密码解码器
+                    //.passwordEncoder(NoOpPasswordEncoder.getInstance())
+                    .passwordEncoder(new BCryptPasswordEncoder())
+                    .withUser("admin").password(new BCryptPasswordEncoder().encode("admin")).roles("ADMIN").and()
+                    .withUser("user").password(new BCryptPasswordEncoder().encode("user")).roles("USER");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
