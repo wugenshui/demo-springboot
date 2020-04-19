fbf4b86e2fa34ef41bb4ea204d7c8435f40e513f
chenbo <963838470@qq.com>
2020-04-19 17:08:37 +0800

    thymeleaf模板

---
+++ oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.demo.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-19
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
---
+++ oauth/spring-security/src/main/java/com/chenbo/demo/spring/security/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.chenbo.demo.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-19
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
