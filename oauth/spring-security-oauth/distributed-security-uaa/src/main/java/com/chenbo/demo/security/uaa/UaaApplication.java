97f6bd243bf96da4210578d28f181f3faaad2f19
chenbo <963838470@qq.com>
2020-04-21 22:01:15 +0800

    oauth2基础架构

---
+++ oauth/spring-security-oauth/distributed-security-uaa/src/main/java/com/chenbo/demo/security/uaa/UaaApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.demo.security.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-21
+ */
+@SpringBootApplication
+public class UaaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UaaApplication.class, args);
+    }
+}
