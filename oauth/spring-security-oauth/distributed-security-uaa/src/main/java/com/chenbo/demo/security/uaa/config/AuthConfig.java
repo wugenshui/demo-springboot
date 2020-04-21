6e76b99e113414fdd66e8a5e4b53e12fb68b4fe4
chenbo <963838470@qq.com>
2020-04-21 22:38:48 +0800

    oauth2 authorization_code授权码模式

---
+++ oauth/spring-security-oauth/distributed-security-uaa/src/main/java/com/chenbo/demo/security/uaa/config/AuthConfig.java
@@ -0,0 +1,37 @@
+package com.chenbo.demo.security.uaa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-21
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * 密码加密工具
+     *
+     * @return
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret(passwordEncoder().encode("secret"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("all")
+                .redirectUris("https://wwww.baidu.com");
+    }
+}
---
+++ oauth/spring-security-oauth/distributed-security-uaa/src/main/java/com/chenbo/demo/security/uaa/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.chenbo.demo.security.uaa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author : chenbo
+ * @date : 2020-04-21
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    /**
+     * 配置登录用户验证
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 基于内存验证
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder.encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder.encode("user")).roles("USER");
+    }
+}
