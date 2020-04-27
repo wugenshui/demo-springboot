399e43ccdbd945d727ebfdc152b5b04102ad636c
chenbo <963838470@qq.com>
2020-04-27 21:59:48 +0800

    获取令牌身份信息

--- best-practice/uaa/src/main/java/com/chenbo/demo/uaa/controller/TestOaurhController.java
+++ best-practice/uaa/src/main/java/com/chenbo/demo/uaa/controller/TestOaurhController.java
@@ -1,6 +1,7 @@
 package com.chenbo.demo.uaa.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +17,22 @@ public class TestOaurhController {
     @GetMapping("/f1")
     @PreAuthorize("hasAnyAuthority('f1')")
     public String f1() {
-        return "访问资源1";
+        return getUser() + "访问资源1";
     }
 
     @GetMapping("/f2")
     @PreAuthorize("hasAnyAuthority('f2')")
     public String f2() {
-        return "访问资源2";
+        return getUser() + "访问资源2";
     }
 
 
     @GetMapping("/f3")
     public String f3() {
-        return "访问资源3";
+        return getUser() + "访问资源3";
+    }
+
+    private String getUser() {
+        return (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
