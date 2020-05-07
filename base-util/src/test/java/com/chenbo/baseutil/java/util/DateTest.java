0c24934c42eed5dba3922352f1b8b6b96e26499f
chenbo <963838470@qq.com>
2020-05-08 00:17:25 +0800

    时间测试类

--- base-util/src/test/java/com/chenbo/baseutil/java/util/DateTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/java/util/DateTest.java
@@ -1,10 +1,11 @@
 package com.chenbo.baseutil.java.util;
 
-import cn.hutool.core.date.DateTime;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -16,12 +17,18 @@ public class DateTest {
     @Test
     public void dateTest() {
         Date date = new Date();
-        System.out.println("date = " + date);
-
-        DateTime dateTime = DateTime.now();
-        System.out.println("dateTime = " + dateTime);
+        System.out.println("date = " + date.toLocaleString());
 
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("localDateTime = " + localDateTime);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2020, 1, 1);
+        date = calendar.getTime();
+        System.out.println("date = " + date.toLocaleString());
+
+        // 默认使用UTC时区
+        Instant instant = Instant.now();
+        System.out.println("instant:" + instant);
     }
 }
