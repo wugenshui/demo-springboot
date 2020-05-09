131fd17e8a31373b1239dd705506e7250587a34d
chenbo <chenbo@huaweisoft.com>
2020-05-09 17:30:29 +0800

    // 解决异常： Zip bomb detected! The file would exceed the max. // 延迟解析比率 ZipSecureFile.setMinInflateRatio(-1.0d);

--- office/poi-tl/src/test/java/com/chenbo/demo/poi/tl/dbWord/DbWordTest.java
+++ office/poi-tl/src/test/java/com/chenbo/demo/poi/tl/dbWord/DbWordTest.java
@@ -8,6 +8,7 @@ import com.deepoove.poi.config.Configure;
 import com.deepoove.poi.data.DocxRenderData;
 import com.deepoove.poi.policy.HackLoopTableRenderPolicy;
 import lombok.val;
+import org.apache.poi.openxml4j.util.ZipSecureFile;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,11 +34,16 @@ public class DbWordTest {
     @Autowired
     private TableMapper mapper;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     @Ignore
-    public void dbWordTest() throws IOException {
+    public void dbWordTest() throws IOException, SQLException {
 
-        List<Tables> allTables = mapper.getAllTables("export");
+        Connection connection = dataSource.getConnection();
+        String dbName = connection.getCatalog();
+        List<Tables> allTables = mapper.getAllTables(dbName);
 
         DbData db = new DbData();
         List<TablePartData> tables = new ArrayList<>();
@@ -53,9 +62,13 @@ public class DbWordTest {
 
         db.setTables(new DocxRenderData(new File("src/test/resources/table.docx"), tables));
 
+        // 解决异常： Zip bomb detected! The file would exceed the max.
+        // 延迟解析比率
+        ZipSecureFile.setMinInflateRatio(-1.0d);
+
         XWPFTemplate.compile("src/test/resources/db.docx", config)
                 .render(db)
-                .writeToFile("src/test/resources/out_db.docx");
+                .writeToFile("src/test/resources/" + dbName + ".docx");
 
     }
 
