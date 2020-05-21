04f87e8f2479b742502c77320078a043d81fe8d1
chenbo <chenbo@huaweisoft.com>
2020-05-21 18:23:15 +0800

    Jedis建立连接

---
+++ db/redis-jedis/src/main/java/com/chenbo/demo/redis/jedis/RedisJedisApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.demo.redis.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-05-21
+ */
+@SpringBootApplication
+public class RedisJedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisJedisApplication.class, args);
+    }
+}
---
+++ db/redis-jedis/src/test/java/com/chenbo/demo/redis/jedis/RedisJedisApplicationTest.java
@@ -0,0 +1,35 @@
+package com.chenbo.demo.redis.jedis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @author : chenbo
+ * @date : 2020-05-21
+ */
+@SpringBootTest
+public class RedisJedisApplicationTest {
+    @Test
+    public void JedisConfigTest() {
+        // 普通连接
+        Jedis jedis = new Jedis("192.168.0.222", 6379);
+        jedis.auth("123456");
+        jedis.set("foo", "bar");
+        String value = jedis.get("foo");
+        Assert.assertEquals("bar", value);
+        System.out.println("value = " + value);
+
+        // 连接池
+        JedisPool jedisPool = new JedisPool("192.168.0.222", 6379);
+        jedis = jedisPool.getResource();
+        jedis.auth("123456");
+        jedis.set("foo", "bar");
+        value = jedis.get("foo");
+        Assert.assertEquals("bar", value);
+        System.out.println("value = " + value);
+
+    }
+}
