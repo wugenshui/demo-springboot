36f2c54ef1510ad26b48c334d18191484125bcbd
chenbo <963838470@qq.com>
2020-05-28 23:03:10 +0800

    redis 事务

--- db/redis-jedis/src/test/java/com/chenbo/demo/redis/jedis/RedisJedisApplicationTest.java
+++ db/redis-jedis/src/test/java/com/chenbo/demo/redis/jedis/RedisJedisApplicationTest.java
@@ -1,10 +1,13 @@
 package com.chenbo.demo.redis.jedis;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 /**
  * @author : chenbo
@@ -30,5 +33,41 @@ public class RedisJedisApplicationTest {
         value = jedis.get("foo");
         Assert.assertEquals("bar", value);
         System.out.println("value = " + value);
+    }
+
+    @Test
+    public void transTest() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", "张三");
+        jsonObject.put("hello", "world");
+
+        JedisPool jedisPool = new JedisPool("192.168.192.170", 6379);
+        Jedis jedis = jedisPool.getResource();
+        jedis.auth("1q2w#E$R");
+
+        // 清空数据
+        jedis.flushDB();
+
+        // 开启事务
+        Transaction multi = jedis.multi();
+
+        try {
+            String result = jsonObject.toString();
+            multi.set("user1", result);
+            // 主动触发异常，事务不会执行
+            int i = 1 / 0;
+            multi.set("user2", result);
+            // 执行事务
+            multi.exec();
+        } catch (Exception e) {
+            // 放弃事务
+            multi.discard();
+            e.printStackTrace();
+        } finally {
+            System.out.println(jedis.get("user1"));
+            System.out.println(jedis.get("user2"));
+            // 关闭连接
+            jedis.close();
+        }
     }
 }
