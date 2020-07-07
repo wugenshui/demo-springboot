92fbe0966f41101f41db936ffa4128965feaa70b
chenbo <chenbo@huaweisoft.com>
2020-07-07 10:43:50 +0800

    ConcurrentHashMap

--- base-util/src/test/java/com/chenbo/baseutil/java/util/MapTest.java
+++ base-util/src/test/java/com/chenbo/baseutil/java/util/MapTest.java
@@ -45,21 +45,21 @@ public class MapTest {
     @Test(expected = NullPointerException.class)
     public void ConTest() {
         ConcurrentHashMap<String, String> map = new ConcurrentHashMap();
+        // 初始化
         map.put("key", "oldValue");
 
+        // 覆盖原有值
         String put = map.put("key", "newValue");
         System.out.println(put);
         System.out.println(map);
 
+        // 如果有值则不修改，如果没有存入设置值
         put = map.putIfAbsent("key", "newValue2");
         System.out.println(put);
         System.out.println(map);
 
+        // key或value为null会抛出空指针异常
         put = map.put("key", null);
-        System.out.println(put);
-        System.out.println(map);
-
-        put = map.putIfAbsent("key", "newValue3");
         System.out.println(put);
         System.out.println(map);
     }
