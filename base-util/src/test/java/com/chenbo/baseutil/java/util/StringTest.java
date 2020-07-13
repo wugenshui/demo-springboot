a184260a47ae99d081dfbee655e4d6a3980e8944
chenbo <chenbo@huaweisoft.com>
2020-07-13 11:14:55 +0800

    字符串切割示例

---
+++ base-util/src/test/java/com/chenbo/baseutil/java/util/StringTest.java
@@ -0,0 +1,30 @@
+package com.chenbo.baseutil.java.util;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author : chenbo
+ * @date : 2020-07-13
+ */
+@SpringBootTest
+public class StringTest {
+    @Test
+    public void SplitTest() {
+        // 想要获取最后一段的字符串
+        String name = "com.baidu.video";
+
+        // split
+        String[] splits = name.split("[.]");
+        if (splits.length > 0) {
+            System.out.println("last = " + splits[splits.length - 1]);
+        }
+
+        // substring
+        int lastIndex = name.lastIndexOf(".");
+        if (lastIndex > -1) {
+            String last = name.substring(lastIndex + 1);
+            System.out.println("last = " + last);
+        }
+    }
+}
