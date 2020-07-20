e20f6918850cc5171979fbe59fa67920ae51bac7
chenbo <chenbo@huaweisoft.com>
2020-07-20 18:44:35 +0800

    activiti依赖管理

---
+++ workflow/activity/src/main/java/com/chenbo/demo/activity/ActivitiApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.demo.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-07-20
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
