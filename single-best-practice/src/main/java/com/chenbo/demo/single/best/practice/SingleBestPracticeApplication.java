3fe6f4c1bdde2030a1959d2dba74085bb29f3486
chenbo <chenbo@huaweisoft.com>
2020-12-23 11:28:02 +0800

    单体项目最佳实践

---
+++ single-best-practice/src/main/java/com/chenbo/demo/single/best/practice/SingleBestPracticeApplication.java
@@ -0,0 +1,17 @@
+package com.chenbo.demo.single.best.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenbo
+ * @date 2020-12-23
+ */
+@SpringBootApplication
+public class SingleBestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleBestPracticeApplication.class, args);
+    }
+
+}
---
+++ single-best-practice/src/test/java/com/chenbo/demo/single/best/practice/SingleBestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenbo.demo.single.best.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleBestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
