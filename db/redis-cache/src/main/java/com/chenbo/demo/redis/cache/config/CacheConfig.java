932dfbe331ff13d484033bf055b29d1758ff9f67
chenbo <chenbo@huaweisoft.com>
2021-01-01 23:45:34 +0800

    自动生成key

--- db/redis-cache/src/main/java/com/chenbo/demo/redis/cache/config/CacheConfig.java
+++ db/redis-cache/src/main/java/com/chenbo/demo/redis/cache/config/CacheConfig.java
@@ -1,5 +1,9 @@
 package com.chenbo.demo.redis.cache.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.interceptor.CacheErrorHandler;
@@ -12,10 +16,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import javax.annotation.Resource;
+
+import java.time.Duration;
 
 import static org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig;
 
@@ -47,14 +53,25 @@ public class CacheConfig extends CachingConfigurerSupport {
     @Bean
     @Override
     public CacheManager cacheManager() {
+        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会抛出异常
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
         RedisCacheConfiguration cacheConfiguration =
                 defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(3))
                         .disableCachingNullValues()
-                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-        return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+        RedisCacheManager build = RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
+        return build;
 
     }
-
+    
     @Override
     @Bean
     public KeyGenerator keyGenerator() {
--- db/redis-cache/src/main/java/com/chenbo/demo/redis/cache/controller/TestController.java
+++ db/redis-cache/src/main/java/com/chenbo/demo/redis/cache/controller/TestController.java
@@ -4,6 +4,7 @@ import com.chenbo.demo.redis.cache.entity.User;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -29,5 +30,11 @@ public class TestController {
         Thread.sleep(2000);
         User user = User.builder().age(18).build();
         return user;
+    }
+
+    @Cacheable("num")
+    @GetMapping("/{num}")
+    public int square(@PathVariable int num) {
+        return num * num;
     }
 }
