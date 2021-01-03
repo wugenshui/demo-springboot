59fcd8bd6dc2261c12ca95b930817e2c2c4b11b4
chenbo <963838470@qq.com>
2021-01-03 23:27:56 +0800

    移位运算符

--- base-util/src/test/java/com/chenbo/baseutil/java/lang/BitwiseOperators.java
+++ base-util/src/test/java/com/chenbo/baseutil/java/lang/BitwiseOperators.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class BitwiseOperators {
     /**
-     * & 对应两个位都为1才为1，否则为0
+     * & 按位或 只要有1出现结果就是1，否则为0
      */
     @Test
     public void andTest() {
@@ -32,7 +32,7 @@ public class BitwiseOperators {
     }
 
     /**
-     * ^ 同 | 类似，但有一点不同的是 如果两个操作位都为1的话，结果产生0
+     * ^ 按位异或 不同为1，相同为0
      */
     @Test
     public void caretTest() {
@@ -42,12 +42,33 @@ public class BitwiseOperators {
     }
 
     /**
-     * ~ 求反
+     * ~ 按位取反 0变1，1变0
      */
     @Test
     public void tildeTest() {
         System.out.println(Integer.toBinaryString(~0b11));
         System.out.println(Integer.toBinaryString(~0b110));
         System.out.println(Integer.toBinaryString(~0b1111));
+    }
+
+    /**
+     * << 左移运算符
+     */
+    @Test
+    public void leftTest() {
+        System.out.println(Integer.toBinaryString(0b11111111 << 2));
+        System.out.println(Integer.toBinaryString(0b00000000 << 2));
+        System.out.println(Integer.toBinaryString(0b11001111 << 2));
+        System.out.println(Integer.toBinaryString(0b11110011 << 2));
+    }
+
+    /**
+     * >> 右移运算符
+     */
+    @Test
+    public void rightTest() {
+        System.out.println(Integer.toBinaryString(0b11 >> 2));
+        System.out.println(Integer.toBinaryString(0b110 >> 2));
+        System.out.println(Integer.toBinaryString(0b1111 >> 2));
     }
 }
