189b4192ca646cd97a2b6bfc742a60f6ad4207b9
chenbo <chenbo@huaweisoft.com>
2021-01-14 22:14:14 +0800

    自定义限流降级策略

--- springcloud/spring-cloud-alibaba/spring-cloud-alibaba-sentinel/src/main/java/com/chenbo/demo/spring/cloud/alibaba/sentinel/controller/TestController.java
+++ springcloud/spring-cloud-alibaba/spring-cloud-alibaba-sentinel/src/main/java/com/chenbo/demo/spring/cloud/alibaba/sentinel/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.chenbo.demo.spring.cloud.alibaba.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class TestController {
 
+    @SentinelResource(value = "test", blockHandler = "exceptionHandler", fallback = "helloFallback")
     @GetMapping
     public String test() {
         return "test";
+    }
+
+    /**
+     * Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+     */
+    public String exceptionHandler(BlockException ex) {
+        ex.printStackTrace();
+        return "异常发生，请求阻断了！";
+    }
+
+    /**
+     * fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+     */
+    public String helloFallback() {
+        return String.format("主动降级！！");
     }
 }
