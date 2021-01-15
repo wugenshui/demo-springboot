bd8cbddae692ea6595b7cc71cd0b24c1da55dae5
chenbo <chenbo@huaweisoft.com>
2021-01-15 15:37:31 +0800

    rocketmq 基础demo

---
+++ springcloud/spring-cloud-alibaba/spring-cloud-alibaba-rocketmq/src/main/java/com/chenbo/demo/spring/cloud/alibaba/rocketmq/RocketmqApplication.java
@@ -0,0 +1,40 @@
+package com.chenbo.demo.spring.cloud.alibaba.rocketmq;
+
+import com.chenbo.demo.spring.cloud.alibaba.rocketmq.producer.SyncProducer;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2021-01-15
+ */
+@SpringBootApplication
+public class RocketmqApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RocketmqApplication.class, args);
+
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("TopicTest");
+        consumer.setVipChannelEnabled(false);
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("192.168.0.222:9876");
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("TopicTest", "*");
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
+            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+            // 标记该消息已经被成功消费
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        // 启动消费者实例
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+
+        SyncProducer.sendMsg();
+    }
+
+}
