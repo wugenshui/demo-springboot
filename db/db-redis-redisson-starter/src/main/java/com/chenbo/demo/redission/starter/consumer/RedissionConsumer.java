301a3bcde905fdf6ad559801d353bc4880099fe0
chenbo <chenbo@huaweisoft.com>
2021-01-19 12:21:16 +0800

    新增线程处理队列读取

--- db/db-redis-redisson-starter/src/main/java/com/chenbo/demo/redission/starter/consumer/RedissionConsumer.java
+++ db/db-redis-redisson-starter/src/main/java/com/chenbo/demo/redission/starter/consumer/RedissionConsumer.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author : chenbo
  * @date : 2021-01-19
@@ -18,13 +22,19 @@ public class RedissionConsumer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        while (true) {
-            try {
-                RBlockingQueue<String> queue = redissonClient.getBlockingQueue("anyQueue");
-                System.out.println("queue.take() = " + queue.take());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        // 新建线程执行逻辑
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        threadPoolExecutor.submit(() -> {
+            while (true) {
+                try {
+                    RBlockingQueue<String> queue = redissonClient.getBlockingQueue("anyQueue");
+                    System.out.println("queue.take() = " + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
     }
 }
