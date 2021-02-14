3b713efb8b3b1f34708f07a56a621477404ef676
chenbo <963838470@qq.com>
2021-02-14 22:29:12 +0800

    单例模式，双重检查

---
+++ test/src/main/java/com/chenbo/baseutil/thread/DubbleSingleton.java
@@ -0,0 +1,23 @@
+package com.chenbo.baseutil.thread;
+
+/**
+ * 单例模式，双重检查
+ *
+ * @author : chenbo
+ * @date : 2021-02-14
+ */
+public class DubbleSingleton {
+
+    private static DubbleSingleton ds;
+
+    public static DubbleSingleton getInstance() {
+        if (ds == null) {
+            synchronized (DubbleSingleton.class) {
+                if (ds == null) {
+                    ds = new DubbleSingleton();
+                }
+            }
+        }
+        return ds;
+    }
+}
