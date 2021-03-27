25437cdf661e5b6b1ae3e563bda49838e1e92d65
chenbo <963838470@qq.com>
2021-03-27 20:20:06 +0800

    文件相关初始化

---
+++ single-best-practice/src/main/java/com/chenbo/demo/single/best/practice/controller/oss/DownloadController.java
@@ -0,0 +1,13 @@
+package com.chenbo.demo.single.best.practice.controller.oss;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : chenbo
+ * @date : 2021-03-27
+ */
+@RestController
+@RequestMapping("/oss/download")
+public class DownloadController {
+}
---
+++ single-best-practice/src/main/java/com/chenbo/demo/single/best/practice/controller/oss/ImageController.java
@@ -0,0 +1,19 @@
+package com.chenbo.demo.single.best.practice.controller.oss;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : chenbo
+ * @date : 2021-03-27
+ */
+@RestController
+@RequestMapping("/oss/image")
+public class ImageController {
+
+    @PostMapping
+    public void saveImage() {
+
+    }
+}
