46351e9a5a36775fcc4f70b06a0e606e9a36cf5b
chenbo <chenbo@huaweisoft.com>
2021-06-03 17:17:22 +0800

    moco 实际运行接口 demo

--- mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
+++ mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
@@ -1,11 +1,22 @@
 package com.chenbo.demo.mock.moco;
 
+import com.github.dreamhead.moco.HttpServer;
+import com.github.dreamhead.moco.Runner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static com.github.dreamhead.moco.Moco.httpServer;
+import static com.github.dreamhead.moco.Runner.runner;
 
 @SpringBootApplication
 public class MockMocoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MockMocoApplication.class, args);
+
+        // 增加mock接口
+        HttpServer server = httpServer(12306);
+        server.response("foo");
+        Runner runner = runner(server);
+        runner.start();
     }
 }
