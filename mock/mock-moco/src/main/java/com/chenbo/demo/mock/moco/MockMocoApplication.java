4ac946b1c372ef538fc9f88df542e563a7fcf147
chenbo <chenbo@huaweisoft.com>
2021-06-03 18:21:15 +0800

    WebSocket mock

--- mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
+++ mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
@@ -1,11 +1,14 @@
 package com.chenbo.demo.mock.moco;
 
 import com.github.dreamhead.moco.HttpServer;
+import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.Runner;
+import com.github.dreamhead.moco.WebSocketServer;
+import com.github.dreamhead.moco.config.MocoFileRootConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.github.dreamhead.moco.Moco.httpServer;
+import static com.github.dreamhead.moco.Moco.*;
 import static com.github.dreamhead.moco.Runner.runner;
 
 @SpringBootApplication
@@ -14,8 +17,19 @@ public class MockMocoApplication {
         SpringApplication.run(MockMocoApplication.class, args);
 
         // 增加mock接口
-        HttpServer server = httpServer(12306);
-        server.response("foo");
+        MocoConfig config = new MocoFileRootConfig("D:\\WorkSpace\\demo-spring-boot\\mock\\mock-moco\\src\\main\\resources\\");
+        config.apply("1.json");
+
+        // Http mock
+        HttpServer server = httpServer(12306, config);
+        server.request(by(uri("/name"))).response(with(text("name")), header("Content-Type", "text/html"));
+        server.request(by(uri("/age"))).response("name");
+        server.response("all");
+
+        // WebSocket mock
+        WebSocketServer websocketServer = server.websocket("/ws");
+        websocketServer.request(by("foo")).response("bar");
+
         Runner runner = runner(server);
         runner.start();
     }
