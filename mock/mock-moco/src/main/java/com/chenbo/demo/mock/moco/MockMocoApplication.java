c7df2c23a338bd3cd77dfd47d1dcb9841a81b0b9
chenbo <chenbo@huaweisoft.com>
2021-06-04 09:20:01 +0800

    去除无用逻辑

--- mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
+++ mock/mock-moco/src/main/java/com/chenbo/demo/mock/moco/MockMocoApplication.java
@@ -1,10 +1,8 @@
 package com.chenbo.demo.mock.moco;
 
 import com.github.dreamhead.moco.HttpServer;
-import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.Runner;
 import com.github.dreamhead.moco.WebSocketServer;
-import com.github.dreamhead.moco.config.MocoFileRootConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,12 +14,8 @@ public class MockMocoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MockMocoApplication.class, args);
 
-        // 增加mock接口
-        MocoConfig config = new MocoFileRootConfig("D:\\WorkSpace\\demo-spring-boot\\mock\\mock-moco\\src\\main\\resources\\");
-        config.apply("1.json");
-
         // Http mock
-        HttpServer server = httpServer(12306, config);
+        HttpServer server = httpServer(12306);
         server.request(by(uri("/name"))).response(with(text("name")), header("Content-Type", "text/html"));
         server.request(by(uri("/age"))).response("name");
         server.response("all");
