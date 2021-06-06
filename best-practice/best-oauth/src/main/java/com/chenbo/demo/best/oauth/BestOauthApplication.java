e9d24548969eab37d050f0cc3ac75290ca3ff779
chenbo <chenbo@huaweisoft.com>
2021-06-06 22:43:32 +0800

    网关服务

--- best-practice/best-oauth/src/main/java/com/chenbo/demo/best/oauth/BestOauthApplication.java
+++ best-practice/best-oauth/src/main/java/com/chenbo/demo/best/oauth/BestOauthApplication.java
@@ -2,12 +2,14 @@ package com.chenbo.demo.best.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author : chenbo
  * @date : 2020-04-25
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BestOauthApplication {
     public static void main(String[] args) {
         SpringApplication.run(BestOauthApplication.class, args);
