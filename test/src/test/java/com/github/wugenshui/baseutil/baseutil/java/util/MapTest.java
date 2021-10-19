4c6eb729088e87cef591422ee96fb8bf8804000e
chenbo <chenbo@huaweisoft.com>
2021-10-19 12:03:20 +0800

    删除多余代码

--- test/src/test/java/com/github/wugenshui/baseutil/baseutil/java/util/MapTest.java
+++ test/src/test/java/com/github/wugenshui/baseutil/baseutil/java/util/MapTest.java
@@ -3,8 +3,6 @@ package com.github.wugenshui.baseutil.baseutil.java.util;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -13,35 +11,6 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @SpringBootTest
 public class MapTest {
-    @Test
-    public void HashMapTest() {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(3, "张三");
-        map.put(4, "李四");
-        map.put(5, "王五");
-
-        // 推荐使用，支持修改，但不支持添加和删除
-        System.out.println("for (Map.Entry<Integer, String> entry : map.entrySet())");
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
-
-        System.out.println("for (Integer i : map.keySet())");
-        for (Integer i : map.keySet()) {
-            System.out.println(i + " -> " + map.get(i));
-        }
-
-        System.out.println("map.forEach((key, value))");
-        map.forEach((key, value) -> {
-            System.out.println(key + " -> " + value);
-        });
-
-        System.out.println("for (String value : map.values())");
-        for (String value : map.values()) {
-            System.out.println(value);
-        }
-    }
-
     @Test(expected = NullPointerException.class)
     public void ConTest() {
         ConcurrentHashMap<String, String> map = new ConcurrentHashMap();
