9e251dccc4d4f6b22e63b19628f3ea41c6c92e63
chenbo <chenbo@huaweisoft.com>
2021-10-28 14:54:46 +0800

    修改为hashmap

--- test/src/test/java/com/github/wugenshui/baseutil/baseutil/data/structure/TireTreeTest.java
+++ test/src/test/java/com/github/wugenshui/baseutil/baseutil/data/structure/TireTreeTest.java
@@ -3,24 +3,22 @@ package com.github.wugenshui.baseutil.baseutil.data.structure;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * TireTree 字典树,  快速找词、字符串分词、词语补全
+ * 新版本，手动修改
  *
  * @author : chenbo
  * @date : 2021-10-27
  */
 @SpringBootTest
-public class TireTreeOld2Test {
+public class TireTreeTest {
     @Test
     public void apiTest() {
         TireTree tree = new TireTree();
 
-        String[] strs = {"azAZ09_", "maiccc", "maid", "ma"};
+        String[] strs = {"azAZ09_", "maiccc", "maid", "ma", "tma"};
         // String[] strs = {"a"};
         for (int i = 0, j = strs.length; i < j; i++) {
             tree.createTireTree(strs[i]);
@@ -33,22 +31,20 @@ public class TireTreeOld2Test {
 }
 
 class TireTree {
-
-    private TreeNode root = new TreeNode(true);
+    private TreeNode root = new TreeNode();
 
     // 在字典树中创建词子树
     public void createTireTree(String str) {
         TreeNode node = root;
         char[] chars = str.toCharArray();
-        int loc = 0;
-
+        char loc = 0;
         for (int i = 0, j = chars.length; i < j; i++) {
             // 字符映射下标
-            loc = getIndex(chars[i]);
-            if (node.childs[loc] == null) {
-                node.childs[loc] = new TreeNode(chars[i], i != chars.length);
+            loc = chars[i];
+            if (node.childs.get(loc) == null) {
+                node.childs.put(loc, new TreeNode());
             }
-            node = node.childs[loc];
+            node = node.childs.get(loc);
         }
         node.isEnd = true;
 
@@ -57,17 +53,17 @@ class TireTree {
     // 单词补齐
     public List<String> wordFill(String str) {
         char[] chars = str.toCharArray();
-        int loc = 0;
+        char loc = 0;
 
         List<String> list = new ArrayList<String>();
         StringBuffer sb = new StringBuffer();
         TreeNode node = root;
         // 找到字符串末字符在字典树中的位置
         for (int i = 0, j = chars.length; i < j; i++) {
-            loc = getIndex(chars[i]);
-            if (node.childs[loc] != null) {
+            loc = chars[i];
+            if (node.childs.get(loc) != null) {
                 sb.append(chars[i]);
-                node = node.childs[loc];
+                node = node.childs.get(loc);
             } else {
                 return null;
             }
@@ -77,61 +73,31 @@ class TireTree {
         Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
         node.str = prefix;
         queue.add(node);
+        TreeNode tempTreeNode = null;
         while (!queue.isEmpty()) {
             node = queue.remove();
             if (node.isEnd) {
                 list.add(node.str);
             }
             if (node.childs != null) {
-                for (int i = 0, j = node.childs.length; i < j; i++) {
-                    if (node.childs[i] != null) {
-                        node.childs[i].str = node.str + node.childs[i].data;
-                        queue.add(node.childs[i]);
-                    }
+                for (Map.Entry<Character, TreeNode> entry : node.childs.entrySet()) {
+                    tempTreeNode = entry.getValue();
+                    tempTreeNode.str = node.str + entry.getKey();
+                    queue.add(tempTreeNode);
                 }
             }
         }
         return list;
     }
-
-    public int getIndex(char c) {
-        int result = 0;
-        if (c >= 'a' && c <= 'z') {
-            result = c - 'a';
-        } else if (c >= 'A' && c <= 'Z') {
-            result = c - 'A' + 26;
-        } else if (c >= '0' && c <= '9') {
-            result = c - '0' + 52;
-        } else {
-            result = 62;
-        }
-        return result;
-    }
 }
 
 class TreeNode {
-    static final int MAX_SIZE = 63;
     String str;
-    char data;
-    TreeNode[] childs;
+    Map<Character, TreeNode> childs;
     boolean isEnd;
 
-    public TreeNode(boolean initChild) {
-        if (initChild) {
-            this.childs = new TreeNode[MAX_SIZE];
-        }
+    public TreeNode() {
+        this.childs = new HashMap<>();
         this.isEnd = false;
-    }
-
-    public TreeNode(char ch, boolean initChild) {
-        if (initChild) {
-            this.childs = new TreeNode[MAX_SIZE];
-        }
-        this.data = ch;
-        this.isEnd = false;
-    }
-
-    public void initChild() {
-        this.childs = new TreeNode[MAX_SIZE];
     }
 }
