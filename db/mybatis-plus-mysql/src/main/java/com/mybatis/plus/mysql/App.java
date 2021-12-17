a679697f02ff07d03f56474ffa23447f31014199
chenbo <963838470@qq.com>
2021-12-17 21:11:45 +0800

    mybatisplus mysql

---
+++ db/mybatis-plus-mysql/src/main/java/com/mybatis/plus/mysql/App.java
@@ -0,0 +1,15 @@
+package com.mybatis.plus.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2021-12-17
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
