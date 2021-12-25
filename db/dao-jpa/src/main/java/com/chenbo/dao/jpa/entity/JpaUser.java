ff98303c9c54d07699de9869037db50e763ae60a
chenbo <963838470@qq.com>
2021-12-25 14:06:47 +0800

    jpa id自增

--- db/dao-jpa/src/main/java/com/chenbo/dao/jpa/entity/JpaUser.java
+++ db/dao-jpa/src/main/java/com/chenbo/dao/jpa/entity/JpaUser.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -16,7 +17,7 @@ import java.time.LocalDateTime;
 public class JpaUser {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
