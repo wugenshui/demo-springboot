0de25ad5138d30c56aab1814db80ca86af9312bc
chenbo <963838470@qq.com>
2021-12-25 22:30:15 +0800

    增加Oauth2认证授权

---
+++ api/knife4j/src/main/java/com/github/wugenshui/knife4j/config/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+package com.github.wugenshui.knife4j.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+ * 授权服务器配置
+ *
+ * @author : chenbo
+ * @date : 2020-04-25
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManagerBean;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 配置令牌端点的访问约束
+        // security
+        //         .tokenKeyAccess("permitAll()")
+        //         .checkTokenAccess("permitAll()")
+        //         .allowFormAuthenticationForClients();
+        super.configure(security);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // 配置客户端详情
+        clients.inMemory()
+                .withClient("client")
+                .secret(passwordEncoder.encode("secret"))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .authorities("user:view");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 配置令牌访问端点和令牌服务
+        endpoints
+                .authenticationManager(authenticationManagerBean)
+                .tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+}
---
+++ api/knife4j/src/main/java/com/github/wugenshui/knife4j/config/WebSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.github.wugenshui.knife4j.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * 授权用户配置
+ *
+ * @author : chenbo
+ * @date : 2020-04-25
+ */
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        // 设置默认的加密方式
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // 认证管理器，密码模式
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 配置用户管理服务
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("123456"))
+                .roles("ADMIN");
+    }
+
+    /**
+     * 配置安全拦截机制
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // OPTIONS请求不需要鉴权
+        http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "/**")
+                .and()
+                .cors().and()
+                .csrf().disable()
+                .antMatcher("/**")
+                .anonymous().and().formLogin();
+        // .anonymous()
+        // .and()
+        // .authorizeRequests()
+        // .antMatchers("/", "/application/check/", "/csrf", "/doc.html", "/v2/api-docs", "/webjars/**", "/swagger-resources/**",
+        //         "/swagger-ui.html", "/oauth/**", "/oauth/authorize", "/doc.html").permitAll()
+        // // 其他接口无权限限制，只需token
+        // .anyRequest().authenticated()
+        // .and()
+        // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+        // .and()
+        // .formLogin();
+    }
+}
