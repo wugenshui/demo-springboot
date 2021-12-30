293ef6a7a1e3956cba5bc356224466787ca6cbb2
chenbo <963838470@qq.com>
2021-12-30 22:05:15 +0800

    client_credentials 模式测试

--- oauth/spring-security-oauth2/src/test/java/com/github/wugenshui/security/oauth2/Oauth2ApplicationTest.java
+++ oauth/spring-security-oauth2/src/test/java/com/github/wugenshui/security/oauth2/Oauth2ApplicationTest.java
@@ -1,19 +1,29 @@
 package com.github.wugenshui.security.oauth2;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 /**
+ * 先运行服务，然后执行单元测试
+ *
  * @author : chenbo
  * @date : 2021-12-29
  */
-@SpringBootTest
 class Oauth2ApplicationTest {
     @Test
     public void oauth2Test() {
-        TestRestTemplate restTemplate = new TestRestTemplate();
-        String response = restTemplate.postForObject("http://client:secret@localhost:8080/oauth/token", null, String.class);
+        // basic身份认证
+        RestTemplate restTemplate = new RestTemplateBuilder().basicAuthentication("client", "secret").build();
+        // 请求参数
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
+        params.add("grant_type", "client_credentials");
+        params.add("scope", "any");
+        // 发起请求
+        JsonNode response = restTemplate.postForObject("http://localhost:8080/oauth/token", params, JsonNode.class);
         System.out.println(response);
     }
 }
