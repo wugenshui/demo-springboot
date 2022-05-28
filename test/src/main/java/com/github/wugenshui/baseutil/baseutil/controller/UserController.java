c6c9f99e64cbaa8391ee52516d5e082109afcb2b
chenbo <963838470@qq.com>
2022-05-28 17:18:34 +0800

    相同bean通过名称区分引用

--- test/src/main/java/com/github/wugenshui/baseutil/baseutil/controller/UserController.java
+++ test/src/main/java/com/github/wugenshui/baseutil/baseutil/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.github.wugenshui.baseutil.baseutil.controller;
 
 import com.github.wugenshui.baseutil.baseutil.entity.AjaxResult;
+import com.github.wugenshui.baseutil.baseutil.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +22,28 @@ import org.springframework.web.bind.annotation.RestController;
 @ApiOperation("用户")
 public class UserController {
 
+    @Bean
+    public User user() {
+        User user = new User();
+        user.setUsername("张三");
+        return user;
+    }
+
+    @Bean
+    public User lisi() {
+        User user = new User();
+        user.setUsername("lisi");
+        return user;
+    }
+
+    @Autowired
+    private User user;
+
+    @Autowired
+    private User lisi;
+
     @GetMapping
     public AjaxResult<String> getUser() {
-        return AjaxResult.success("返回用户信息");
+        return AjaxResult.success("返回用户信息" + user.getUsername() + lisi.getUsername());
     }
 }
