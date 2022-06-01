8c9685e8b508684e08e28e400b26f1bb4f7ca87e
chenbo <963838470@qq.com>
2022-06-01 23:04:47 +0800

    默认启动项目缩减

--- api/webflux/src/main/java/com/github/wugenshui/webflux/WebFluxApplication.java
+++ api/webflux/src/main/java/com/github/wugenshui/webflux/WebFluxApplication.java
@@ -10,9 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebFluxApplication {
     public static void main(String[] args) {
-
-
-
         SpringApplication.run(WebFluxApplication.class, args);
     }
 }
