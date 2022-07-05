7a42b922b17a3ff83725693df4fb54ff5fc91474
chenbo <963838470@qq.com>
2022-07-05 22:25:57 +0800

    spring cloud gateway

---
+++ springcloud/spring-cloud-gateway/src/test/java/com/github/wugenshui/spring/cloud/gateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wugenshui.spring.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
