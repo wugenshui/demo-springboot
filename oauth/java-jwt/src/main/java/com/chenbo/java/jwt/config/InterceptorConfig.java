e12a487f224953c79640d70b9750e5e0b134787d
chenbo <963838470@qq.com>
2022-07-07 23:37:06 +0800

    拦截请求

--- oauth/java-jwt/src/main/java/com/chenbo/java/jwt/config/InterceptorConfig.java
+++ oauth/java-jwt/src/main/java/com/chenbo/java/jwt/config/InterceptorConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录
+        // 拦截所有请求，通过判断注解 决定是否需要登录
         registry.addInterceptor(authenticationInterceptor())
                 .addPathPatterns("/**");
     }
