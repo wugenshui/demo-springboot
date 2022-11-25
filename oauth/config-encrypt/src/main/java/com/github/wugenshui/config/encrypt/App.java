af74e835ffb3d19b8c5592d9b8ecc104d85d5853
chenbo <963838470@qq.com>
2022-11-25 20:16:14 +0800

    jasypt-spring-boot-starter + @EnableEncryptableProperties +  jasypt.encryptor.password

---
+++ oauth/config-encrypt/src/main/java/com/github/wugenshui/config/encrypt/App.java
@@ -0,0 +1,23 @@
+package com.github.wugenshui.config.encrypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author : chenbo
+ * @date : 2022-11-25
+ */
+@SpringBootApplication
+@EnableEncryptableProperties
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext app = SpringApplication.run(App.class, args);
+        ConfigurableEnvironment environment = app.getEnvironment();
+        System.out.println("aa:" + environment.getProperty("aa"));
+        System.out.println("ea:" + environment.getProperty("ea"));
+        System.out.println("eb:" + environment.getProperty("eb"));
+    }
+}
