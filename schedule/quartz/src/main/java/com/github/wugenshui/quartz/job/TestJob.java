286a23ad7901c5d2f2e3d0f20b3e1514c272b6a6
chenbo <chenbo@huaweisoft.com>
2023-08-22 17:04:42 +0800

    quartz 获取任务详情

--- schedule/quartz/src/main/java/com/github/wugenshui/quartz/job/TestJob.java
+++ schedule/quartz/src/main/java/com/github/wugenshui/quartz/job/TestJob.java
@@ -1,6 +1,7 @@
 package com.github.wugenshui.quartz.job;
 
 import org.quartz.Job;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,14 @@ import java.time.format.DateTimeFormatter;
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
+        // 获取任务详情
+        JobDetail jobDetail = jobExecutionContext.getJobDetail();
+        String name = jobDetail.getKey().getName();
+        String group = jobDetail.getKey().getGroup();
+        String jobName = jobDetail.getJobClass().getName();
+        System.out.println("jobName = " + jobName + ", name = " + name + ", group = " + group);
+
+        // 定时任务具体执行逻辑
         String data = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println("开启数据备份，当前时间：" + data);
     }
