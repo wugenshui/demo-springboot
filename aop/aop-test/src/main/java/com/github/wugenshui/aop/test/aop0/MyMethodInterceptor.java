36845b78d69d33653f623985d6699d3b7f7fec59
chenbo <chenbo@huaweisoft.com>
2023-11-13 17:51:24 +0800

    获取注解的配置

--- aop/aop-test/src/main/java/com/github/wugenshui/aop/test/aop0/MyMethodInterceptor.java
+++ aop/aop-test/src/main/java/com/github/wugenshui/aop/test/aop0/MyMethodInterceptor.java
@@ -1,8 +1,12 @@
 package com.github.wugenshui.aop.test.aop0;
 
+import com.github.wugenshui.aop.test.aop0.annotation.DataPermission;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.core.annotation.AnnotationUtils;
+
+import java.lang.reflect.Method;
 
 /**
  * 自定义方法拦截器
@@ -14,6 +18,17 @@ import org.aopalliance.intercept.MethodInvocation;
 public class MyMethodInterceptor implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
+        // 获取注解的配置
+        Method method = invocation.getMethod();
+        Object targetObject = invocation.getThis();
+        Class<?> clazz = targetObject != null ? targetObject.getClass() : method.getDeclaringClass();
+        DataPermission dataPermission = AnnotationUtils.findAnnotation(method, DataPermission.class);
+        if (dataPermission == null) {
+            dataPermission = AnnotationUtils.findAnnotation(clazz, DataPermission.class);
+        }
+        log.info("dataPermission.enable()=" + dataPermission.enable());
+
+
         log.info("MyMethodInterceptor：请求方法={}, 请求参数={}", invocation.getStaticPart(), invocation.getArguments());
         Object proceed = invocation.proceed();
         if (proceed instanceof String) {
