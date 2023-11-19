e91fc456d648b7e34a8cd0c8e70078fe497b2839
chenbo <963838470@qq.com>
2023-11-19 11:17:35 +0800

    优化效率

--- db/mybatis-plus-mysql/src/main/java/com/mybatis/plus/mysql/sims/entity/Class.java
+++ db/mybatis-plus-mysql/src/main/java/com/mybatis/plus/mysql/sims/entity/Class.java
@@ -1,5 +1,6 @@
 package com.mybatis.plus.mysql.sims.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
@@ -22,7 +23,7 @@ public class Class implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId
+    @TableId(type = IdType.NONE)
     @ApiModelProperty("班级ID")
     private String classId;
 
--- db/mybatis-plus-mysql/src/test/java/com/mybatis/plus/mysql/sims/service/BatchSaveTest.java
+++ db/mybatis-plus-mysql/src/test/java/com/mybatis/plus/mysql/sims/service/BatchSaveTest.java
@@ -23,7 +23,7 @@ public class BatchSaveTest {
     @Test
     void insertTest() {
         StopWatch stopWatch = new StopWatch();
-        int size = 5;
+        int size = 50000;
         stopWatch.start("初始化数据:" + size);
         List<Class> classes = new ArrayList<>(5000);
         for (int i = 0; i < size; i++) {
@@ -36,12 +36,20 @@ public class BatchSaveTest {
 
         stopWatch.start("批量插入");
         // saveBatch 是简写，但是实际上数据仍是一条一条插入的
+        // 优化1. 批量插入表主键 @TableId(type = IdType.NONE)             5万条12.508s
+        // 优化2. YAML配置文件增加配置 mybatis-plus.executorType: BATCH   5万条12.352s
+        // 优化3. 数据库连接字符串增加参数 &rewriteBatchedStatements=true 5万条6.470s
         boolean state = classService.saveBatch(classes);
         stopWatch.stop();
         System.out.println("save state:" + state);
 
-        stopWatch.start("删除数据");
+        stopWatch.start("查询数据");
         QueryWrapper queryWrapper = new QueryWrapper();
+        List list = classService.list(queryWrapper);
+        System.out.println("query count:" + list.size());
+        stopWatch.stop();
+
+        stopWatch.start("删除数据");
         classService.remove(queryWrapper);
         stopWatch.stop();
 
