56e2c50a5331b4744149cf405499f90e4ec5a7ec
chenbo <chenbo@huaweisoft.com>
2023-11-27 18:52:04 +0800

    缓存key规则

--- db/redis-cache/src/main/java/com/github/wugenshui/redis/cache/controller/TestController.java
+++ db/redis-cache/src/main/java/com/github/wugenshui/redis/cache/controller/TestController.java
@@ -28,6 +28,8 @@ public class TestController {
 
     /**
      * 固定主键缓存:   cache:user::com.github.wugenshui.redis.cache.controller.TestControllertest
+     * 1. 当第一次调用时，会将方法的返回值作为value存入缓存中
+     * 2. 再次调用此方法时，会从缓存中取出数据，而不会再次执行方法
      */
     @Cacheable(value = "user")
     @GetMapping
@@ -42,6 +44,7 @@ public class TestController {
 
     /**
      * 固定主键缓存:   cache:user::com.github.wugenshui.redis.cache.controller.TestControllerput
+     * 1. 每次调用时，都会将方法的返回值作为value存入缓存中
      */
     @CachePut(value = "put")
     @GetMapping("/put")
@@ -55,18 +58,36 @@ public class TestController {
         return user;
     }
 
+    /**
+     * 参数作为缓存的key：      cache:num::1
+     */
     @Cacheable(value = "num", key = "#num")
     @GetMapping("/{num}")
     public String square(@PathVariable int num) {
         return "num * num = " + (num * num);
     }
 
+    /**
+     * 删除单个缓存：      cache:num::1
+     */
     @CacheEvict(value = "num", key = "#num")
     @GetMapping("/delete/{num}")
     public String delete(@PathVariable int num) {
         return "delete:" + num;
     }
 
+    /**
+     * 删除全部缓存：      cache:num::*
+     */
+    @CacheEvict(value = "num", allEntries = true)
+    @GetMapping("/delete")
+    public String deleteAll() {
+        return "deleteAll";
+    }
+
+    /**
+     * 多个参数作为缓存的key：      cache:ab::com.github.wugenshui.redis.cache.controller.TestControllersquare-2-1
+     */
     @Cacheable("ab")
     @GetMapping("/{a}/{b}")
     public String square(@PathVariable int a, @PathVariable int b) {
