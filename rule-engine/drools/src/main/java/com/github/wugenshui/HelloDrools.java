71edb6cecfd9293cba160776a2266edaefb02370
chenbo <chenbo@huaweisoft.com>
2024-01-21 15:47:43 +0800

    hello drools

---
+++ rule-engine/drools/src/main/java/com/github/wugenshui/HelloDrools.java
@@ -0,0 +1,50 @@
+package com.github.wugenshui;
+
+import lombok.Data;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * @author : chenbo
+ * @date : 2024-01-21
+ */
+public class HelloDrools {
+    public static void main(String[] args) {
+        // 初始化
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
+        // 构建 fact
+        User user = new User();
+        user.setName("taven");
+        user.setPoint(10D);
+        user.setLevel(5);
+        user.setPrice(100D);
+        user.setAge(19);
+
+        Order order = new Order();
+        order.setPrice(58D);
+        // insert fact
+        kieSession.insert(user);
+        kieSession.insert(order);
+        // 触发所有规则
+        int fireCount = kieSession.fireAllRules();
+        System.out.println("fireRuleCount:" + fireCount);
+        kieSession.dispose();
+    }
+
+    @Data
+    public static class Order {
+        private Double price;
+    }
+
+    @Data
+    public static class User {
+        private String name;
+        private Integer age;
+        private Double price;
+        private Integer level;
+        private Double point;
+    }
+}
