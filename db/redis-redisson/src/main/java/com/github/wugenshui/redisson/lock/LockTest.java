873b2a07dc0c1b6b467ed19ef9caa960fdb93d71
chenbo <chenbo@huaweisoft.com>
2024-04-09 16:08:14 +0800

    分布式锁测试

--- db/redis-redisson/src/main/java/com/github/wugenshui/redisson/lock/LockTest.java
+++ db/redis-redisson/src/main/java/com/github/wugenshui/redisson/lock/LockTest.java
@@ -1,18 +1,20 @@
 package com.github.wugenshui.redisson.lock;
 
-import lombok.SneakyThrows;
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
 /**
+ * 分布式锁测试
+ * 利用 redis hash 实现
+ *
  * @author : chenbo
  * @date : 2020-05-31
  */
 public class LockTest {
 
-    public static void test() throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         Config config = new Config();
 
         // 集群模式
@@ -22,26 +24,28 @@ public class LockTest {
 
         // 单机模式
         config.useSingleServer()
-                //.setAddress("redis://192.168.192.170:6379")
-                .setAddress("redis://192.168.0.222:6379")
-                .setPassword("123456");
+                .setAddress("redis://localhost:6379");
+        //.setAddress("redis://192.168.0.222:6379")
+        //.setPassword("123456");
 
-        RedissonClient redisson = Redisson.create(config);
+        final RedissonClient redisson = Redisson.create(config);
 
         for (int i = 0; i < 5; i++) {
             new Thread(new Runnable() {
-                @SneakyThrows
-                @Override
                 public void run() {
                     RLock lock = redisson.getLock("myLock");
                     System.out.println(Thread.currentThread().getName() + " 获取锁对象");
                     lock.lock();
                     System.out.println(Thread.currentThread().getName() + " 获取锁");
-                    Thread.sleep(3000);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                     lock.unlock();
                     System.out.println(Thread.currentThread().getName() + " 释放锁");
                 }
-            }, "线程" + (i + 1)).start();
+            }).start();
         }
     }
 }
