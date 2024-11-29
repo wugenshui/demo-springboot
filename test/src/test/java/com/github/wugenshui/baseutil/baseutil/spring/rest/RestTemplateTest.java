48a57cdc0b20f4b50a180223a2e8f2882e6e4a1d
chenbo <chenbo@huaweisoft.com>
2024-11-29 18:10:31 +0800

    兼容junit4和5

--- test/src/test/java/com/github/wugenshui/baseutil/baseutil/spring/rest/RestTemplateTest.java
+++ test/src/test/java/com/github/wugenshui/baseutil/baseutil/spring/rest/RestTemplateTest.java
@@ -1,14 +1,12 @@
 package com.github.wugenshui.baseutil.baseutil.spring.rest;
 
 import cn.hutool.core.io.FileUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -21,22 +19,21 @@ import java.io.File;
  * @date : 2024-11-29
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
-public class RestTemplateTest {
+class RestTemplateTest {
     @Resource
     private RestTemplate restTemplate;
 
     @Test
-    public void test() {
+    void test() {
         // 接口地址
         String url = "http://192.168.0.89:8082/office/convert/pdf";
 
         // 模拟本地文件
-        String filePath = "C:\\Users\\xxxx\\Documents\\中华人民共和国民法典.docx";
+        String filePath = "C:\\Users\\chenbo\\Documents\\中华人民共和国民法典.docx";
         File file = new File(filePath);
 
         // 构造参数发起请求
-        MultiValueMap<String, Object> params  = new LinkedMultiValueMap<>();
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
         // 添加上传文件
